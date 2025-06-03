1701ef20b49cdff867671c7ee405c351728a8e7e
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2025-06-02 21:16:13 -0300

    Create some simple calculator

--- app/main.go
+++ app/main.go
@@ -2,11 +2,34 @@ package main
 
 import "fmt"
 
-func sum(a, b int) int {
-    return a + b
-}
-
 func main() {
-    fmt.Println("Hello, Go!")
-    fmt.Println("2 + 3 =", sum(2, 3))
+	var a, b int
+	var operation string
+
+	fmt.Print("Digite o primeiro número: ")
+	fmt.Scanln(&a)
+
+	fmt.Println("Escolha uma operação: soma(+), subtração(-), multiplicação(*), divisão(/)")
+	fmt.Scanln(&operation)
+
+	fmt.Print("Digite o segundo número: ")
+	fmt.Scanln(&b)
+
+	switch operation {
+	case "+":
+		fmt.Printf("Resultado: %d\n", a+b)
+	case "-":
+		fmt.Printf("Resultado: %d\n", a-b)
+	case "*":
+		fmt.Printf("Resultado: %d\n", a*b)
+	case "/":
+		if b != 0 {
+			fmt.Printf("Resultado: %d\n", a/b)
+		} else {
+			fmt.Println("Erro: Divisão por zero não é permitida.")
+		}
+	default:
+		fmt.Println("Operação inválida. Por favor, escolha entre +, -, * ou /.")
+		return
+	}
 }
