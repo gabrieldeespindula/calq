733e64d12fcf5f4d9ee6a47fa1c25803daf9a272
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2025-06-02 21:51:19 -0300

    Add loop

--- app/main.go
+++ app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"errors"
+	"strings"
 )
 
 func sum(a, b int) (int, error) {
@@ -39,32 +40,43 @@ func main() {
 		DIV: divide,
 	}
 
-	var firstNumber, secondNumber int
-	var operation string
+	for {
 
-	fmt.Print("Type the first number: ")
-	fmt.Scanln(&firstNumber)
+		var firstNumber, secondNumber int
+		var operation string
 
-	fmt.Println("Choose an operation: addition(+), subtraction(-), multiplication(*), division(/)")
-	fmt.Scanln(&operation)
+		fmt.Print("Type the first number: ")
+		fmt.Scanln(&firstNumber)
 
-	fmt.Print("Type the second number: ")
-	fmt.Scanln(&secondNumber)
+		fmt.Println("Choose an operation: addition(+), subtraction(-), multiplication(*), division(/)")
+		fmt.Scanln(&operation)
 
-	opFunc, ok := operations[operation]
+		fmt.Print("Type the second number: ")
+		fmt.Scanln(&secondNumber)
 
-	if !ok {
-		fmt.Println("Invalid operation. Please choose one of the following: +, -, *, /")
-		return
+		opFunc, ok := operations[operation]
+
+		if !ok {
+			fmt.Println("Invalid operation. Please choose one of the following: +, -, *, /")
+			return
+		}
+
+		result, err := opFunc(firstNumber, secondNumber)
+		if err != nil {
+			fmt.Println("Error:", err)
+			fmt.Println("Please try again with valid numbers")
+			return
+		}
+
+		fmt.Printf("The result of %d %s %d is: %d\n", firstNumber, operation, secondNumber, result)
+		
+		var choice string
+		fmt.Print("Do you want to calculate again? (y/n): ")
+		fmt.Scanln(&choice)
+
+		if strings.ToLower(choice) != "y" {
+			fmt.Println("Thank you for using the calculator!")
+			break
+		}
 	}
-
-	result, err := opFunc(firstNumber, secondNumber)
-	if err != nil {
-		fmt.Println("Error:", err)
-		fmt.Println("Please try again with valid numbers")
-		return
-	}
-
-	fmt.Printf("The result of %d %s %d is: %d\n", firstNumber, operation, secondNumber, result)
-	fmt.Println("Thank you for using the calculator!")
 }
