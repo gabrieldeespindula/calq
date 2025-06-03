f4a92aeb5b484d189d3077afa3fef28a13c623f6
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2025-06-02 21:43:55 -0300

    Calculator 2.0, I am learning somethings

--- app/main.go
+++ app/main.go
@@ -1,35 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
+
+func sum(a, b int) (int, error) {
+	return a + b, nil
+}
+
+func subtract(a, b int) (int, error) {
+	return a - b, nil
+}
+
+func multiply(a, b int) (int, error) {
+	return a * b, nil
+}
+
+func divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("divisão por zero não permitida")
+	}
+	return a / b, nil
+}
 
 func main() {
-	var a, b int
+	const (
+		ADD = "+"
+		SUB = "-"
+		MUL = "*"
+		DIV = "/"
+	)
+
+	operations := map[string]func(int, int) (int, error){
+		ADD: sum,
+		SUB: subtract,
+		MUL: multiply,
+		DIV: divide,
+	}
+
+	var firstNumber, secondNumber int
 	var operation string
 
-	fmt.Print("Digite o primeiro número: ")
-	fmt.Scanln(&a)
+	fmt.Print("Type the first number: ")
+	fmt.Scanln(&firstNumber)
 
-	fmt.Println("Escolha uma operação: soma(+), subtração(-), multiplicação(*), divisão(/)")
+	fmt.Println("Choose an operation: addition(+), subtraction(-), multiplication(*), division(/)")
 	fmt.Scanln(&operation)
 
-	fmt.Print("Digite o segundo número: ")
-	fmt.Scanln(&b)
+	fmt.Print("Type the second number: ")
+	fmt.Scanln(&secondNumber)
 
-	switch operation {
-	case "+":
-		fmt.Printf("Resultado: %d\n", a+b)
-	case "-":
-		fmt.Printf("Resultado: %d\n", a-b)
-	case "*":
-		fmt.Printf("Resultado: %d\n", a*b)
-	case "/":
-		if b != 0 {
-			fmt.Printf("Resultado: %d\n", a/b)
-		} else {
-			fmt.Println("Erro: Divisão por zero não é permitida.")
-		}
-	default:
-		fmt.Println("Operação inválida. Por favor, escolha entre +, -, * ou /.")
+	opFunc, ok := operations[operation]
+
+	if !ok {
+		fmt.Println("Operação inválida! Por favor escolha +, -, * ou /")
 		return
 	}
+
+	result, err := opFunc(firstNumber, secondNumber)
+	if err != nil {
+		fmt.Println("Erro:", err)
+		return
+	}
+
+	fmt.Printf("Resultado: %d\n", result)
 }
