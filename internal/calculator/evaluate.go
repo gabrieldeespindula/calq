5fefc4f1005a85a0e3fc9830b490e8e8035cd993
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2025-06-11 13:31:06 -0300

    refactor(tokenize): extract code to func to describe it better

--- internal/calculator/evaluate.go
+++ internal/calculator/evaluate.go
@@ -10,30 +10,18 @@ import (
 
 func Tokenize(expr, lastResult string) ([]string, error) {
 	expr = strings.TrimSpace(expr)
+	if expr == "" {
+		return nil, errors.New("empty expression")
+	}
+
 	if lastResult != "" && strings.ContainsAny(string(expr[0]), ADD+SUB+MUL+DIV) {
 		expr = lastResult + expr
 	}
 
-	re := regexp.MustCompile(`\d+`)
-	expr = re.ReplaceAllStringFunc(expr, func(match string) string {
-		return "#" + match + "#"
-	})
-
-	replacer := strings.NewReplacer(
-		ADD, " "+ADD+" ",
-		MUL, " "+MUL+" ",
-		DIV, " "+DIV+" ",
-		SUB, " "+SUB+" ",
-	)
-
-	expr = replacer.Replace(expr)
-
-	replacer = strings.NewReplacer(
-		"  " + SUB + " #", " "+SUB,
-		"#", "",
-	)
-
-	expr = replacer.Replace(expr)
+	expr = delimitNumbers(expr)
+	expr = addSpaceBetweenOperators(expr)
+	expr = handleNegativeNumbers(expr)
+	expr = cleanNumbersDelimiters(expr)
 
 	tokens := strings.Fields(expr)
 
@@ -41,12 +29,38 @@ func Tokenize(expr, lastResult string) ([]string, error) {
 		return nil, errors.New("empty expression")
 	}
 
-	if len(tokens) > 0 && tokens[0] == "-" {
-		newToken := "-" + tokens[1]
-		tokens = append([]string{newToken}, tokens[2:]...)
+	if tokens[0] == SUB && len(tokens) > 1 {
+		tokens = handleNegativeNumbersAtStart(tokens)
 	}
 
 	return tokens, nil
+}
+
+func delimitNumbers(expr string) string {
+	return regexp.MustCompile(`\d+`).ReplaceAllStringFunc(expr, func(m string) string {
+		return "#" + m + "#"
+	})
+}
+
+func addSpaceBetweenOperators(expr string) string {
+	for _, op := range []string{ADD, SUB, MUL, DIV} {
+		expr = strings.ReplaceAll(expr, op, " "+op+" ")
+	}
+
+	return expr
+}
+
+func handleNegativeNumbers(expr string) string {
+	return strings.ReplaceAll(expr, "  "+SUB+" #", " "+SUB+"")
+}
+
+
+func cleanNumbersDelimiters(expr string) string {
+	return strings.ReplaceAll(expr, "#", "")
+}
+
+func handleNegativeNumbersAtStart(tokens []string) []string {
+	return append([]string{tokens[0] + tokens[1]}, tokens[2:]...)
 }
 
 func Evaluate(parts []string) (float64, error) {
